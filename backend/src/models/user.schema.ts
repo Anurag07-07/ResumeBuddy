a62f453e0946eeaf8a1f85c5332568ed2cfbd1f5
Anurag07-07 <adityanv8@gmail.com>
2026-03-13 19:32:14 +0530

    usermodel created

---
+++ backend/src/models/user.schema.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, type Document } from "mongoose";
+
+export interface IUser extends Document{
+  username:string,
+  email:string
+  password:string
+}
+
+export const UserModel:Schema = new Schema({
+  username:{
+    type:String,
+    unique:[true,"username already present"],
+    required:true
+  },
+  email:{
+    type:String,
+    unique:[true,"Account already exists with this email address"],
+    required:true,
+  },
+  password:{
+    type:String,
+    required:true
+  }
+})
+
+export default mongoose.model<IUser>("Users",UserModel)
