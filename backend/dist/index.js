b7d28ecedbdf3a8fc17b262decf3b9289c8c8f2b
Anurag07-07 <adityanv8@gmail.com>
2026-03-16 21:25:43 +0530

    Configure CORS to allow specific origins and enable credentials

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -6,7 +6,10 @@ import { dbConnect } from "./db/db.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+}));
 app.use(cookieParser());
 import userauth from "./routes/auth.route.js";
 import { connectRedis } from "./db/redisClient.js";
